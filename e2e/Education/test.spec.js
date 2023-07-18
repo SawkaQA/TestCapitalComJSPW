c881cb68cc43be58e0c0e974fbfaa71c019c47c8
SawkaQA <aleksandr.sdobnov.qa@gmail.com>
2023-07-18 22:36:37 +0300

    upd: remove allure results

--- e2e/Education/test.spec.js
+++ e2e/Education/test.spec.js
@@ -1,4 +1,5 @@
 const { test, expect } = require("@playwright/test");
+import { allure } from "allure-playwright";
 const { Header } = require("../../pages/header");
 // import headerData from "../../fixtures/headerData.json";
 
@@ -21,13 +22,9 @@ test.describe("US_11.03.05 | Educations > Menu item [Swing Trading]", () => {
     });
 
     languages.forEach((language) => {
-        test(`Chech route on Education Menu on ${language.langName}`, async ({
-            page,
-        }) => {
+        test(`Check route on Education Menu on ${language.langName}`, async ({ page }) => {
             const header = new Header(page);
             await page.locator(language.langUrl).click();
-            const lang = page.getByRole('link', { name: 'English' })
-            const title = page.locator('h1[class="hero"]');
             await header.clickEducationMenu();
         });
 
@@ -35,13 +32,17 @@ test.describe("US_11.03.05 | Educations > Menu item [Swing Trading]", () => {
             await test.step("Hover on Education menu and select language", async () => {
                 const header = new Header(page);
                 await page.locator(language.langUrl).click();
-                const lang = page.getByRole('link', { name: 'العَرَبِيَّة' })
                 await header.getEducationMenu.hover();
                 const isVisibleSwing = page.locator('[class="cc-nav__dropdown gridRUp gXs "] [href="https://capital.com/swing-trading"]');
 
                 if (await isVisibleSwing.isVisible()) {
                     await isVisibleSwing.click();
+                } else {
+                    await test.step('Check visible links on page for languages', async () => {
+                        console.log(`For test language the page "Education->Position Trading" doesn't exist on production`)
+                    test.skip()
                 }
+            )}
             });
         });
     });
