3921639a419b649d152a942b5bd6777a2ee0573d
SawkaQA <aleksandr.sdobnov.qa@gmail.com>
2023-07-19 22:04:18 +0300

    upd: class

--- e2e/Education/test.spec.js
+++ e2e/Education/test.spec.js
@@ -1,7 +1,5 @@
 const { test, expect } = require("@playwright/test");
-import { allure } from "allure-playwright";
 const { Header } = require("../../pages/header");
-// import headerData from "../../fixtures/headerData.json";
 
 test.describe("US_11.03.05 | Educations > Menu item [Swing Trading]", () => {
 
@@ -19,30 +17,21 @@ test.describe("US_11.03.05 | Educations > Menu item [Swing Trading]", () => {
 
     test.beforeEach(async ({ page }) => {
         header = new Header(page);
-        // open capital.com
         await page.goto("/");
         await page.locator("div .licLangSw__btn").hover();
     });
 
     languages.forEach((language) => {
-        test(`Check route on Education Menu on ${language.langName}`, async ({
-            page,
-        }) => {
-            // const header = new Header(page);
+        test(`Check route on Education Menu on ${language.langName}`, async ({ page }) => {
             await page.locator(language.langUrl).click();
             await header.clickEducationMenu();
         });
 
-        test(`Click on Swing Trading ddsds on ${language.langName}`, async ({
-            page,
-        }) => {
+        test(`Click on Swing Trading ddsds on ${language.langName}`, async ({ page }) => {
             await test.step("Hover on Education menu and select language", async () => {
-                // const header = new Header(page);
                 await page.locator(language.langUrl).click();
                 await header.getEducationMenu.hover();
-                const isVisibleSwing = page.locator(
-                    '[class="cc-nav__dropdown gridRUp gXs "] [href="https://capital.com/swing-trading"]'
-                );
+                const isVisibleSwing = page.locator('[class="cc-nav__dropdown gridRUp gXs "] [href="https://capital.com/swing-trading"]');
 
                 if (await isVisibleSwing.isVisible()) {
                     await isVisibleSwing.click();
