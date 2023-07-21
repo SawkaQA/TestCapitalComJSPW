8734c5ce92c65f3cf08cb3e4d1a6c6679a1cd0b5
Alexeeva Viktoria <tori-r@mail.ru>
2023-07-21 20:49:32 +0200

    us_11-03-04-08

--- pages/login.js
+++ pages/login.js
@@ -4,6 +4,8 @@ class LoginPage {
       this.userName = page.locator("#l_f_email > .field__control");
       this.password = page.locator("#l_f_pass > .field__control");
       this.continueButton = page.locator(".form-container-white > .form-container-small-content > form > .btn");
+      this.BtnLogIn = page.locator("#wg_loginBtn")
+      this.FormLogIn = page.locator("#l_overlay > .form-container-white");
     }
   
     async visit() {
@@ -15,5 +17,10 @@ class LoginPage {
       await this.password.fill(password);
       await this.continueButton.click();
     }
+
+    async clickBtnLogIn() {
+      await this.BtnLogIn.click();
+  }
+
   }
   module.exports = { LoginPage };
