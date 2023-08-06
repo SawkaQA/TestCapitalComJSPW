65b717fe41ed2d83d7f128018b53f1067454c39b
Alexeeva Viktoria <tori-r@mail.ru>
2023-08-06 21:11:25 +0200

    rf_11-02-02_01(pl)

--- e2e/Education/US_11-02-02.spec.js
+++ e2e/Education/US_11-02-02.spec.js
@@ -5,8 +5,8 @@ const { BannerBtn } = require("../../pages/bannerButtons")
 let header;
 let page;
 let bannerBtn;
-const language = "English";
-const country = "United Kingdom";
+const language = "Polski";
+const country = "Poland";
 
 test.describe("US_11-02-02_Education > Menu item [Share trading] on UnReg Role", () => {
 
@@ -32,26 +32,53 @@ test.describe("US_11-02-02_Education > Menu item [Share trading] on UnReg Role",
   test(`TC_11.02.02_01_UnReg  > Test button [Start Trading] in Main banner on '${language}' language`, async () => {
     bannerBtn = new BannerBtn(page);
     const fs = require('fs');
+    await page.waitForTimeout(20000);
     const links = await page.$$eval('a[data-type="sidebar_deeplink"]', (elements) => elements.map((el) => el.href));
-    await fs.promises.writeFile('links.txt', links.join('\n'));
-    const fileContent = await fs.promises.readFile('links.txt', 'utf-8');
+    console.log("links", links);
+    fs.writeFileSync('links.txt', links.join('\n'));
+    const fileContent = fs.readFileSync('links.txt', 'utf-8');
     const linksFromFile = fileContent.split('\n').filter((link) => link !== '');
-    const randomLinks = getRandomElements(linksFromFile, 3);
+    const randomLinks = getRandomElements(linksFromFile, 4);
     await page.goto(linksFromFile[0]);
     await bannerBtn.clickStartTradingBtnOnMainBanner();
+    await expect(page.locator("#s_overlay > .form-container-white")).toBeVisible();
+    const elementText = await page.$eval('#s_overlay', element => element.innerText);
+    expect(elementText).toBeTruthy();
+    await expect(page.locator('[class="signup-form"] .h1')).toBeVisible();
+    await expect(page.locator('#s_overlay-email > .field__control')).toHaveAttribute("type", "email");
+    await expect(page.locator('#s_overlay-pass > .field__control')).toHaveAttribute("type", "password");
+    await expect(page.locator('.signup-form > .form-container-small-content > form > .btn')).toBeVisible();
+    await expect(page.locator('a[href="/terms-and-policies"]')).toBeVisible();
+    await page.locator('#s_overlay .form-container-white .button-cleared').click();
+    if (page.url() === linksFromFile[0]) {
+      console.log(`Testing on the '${linksFromFile[0]}' link was successfully completed `);
+    } else {
+      console.log(`Testing on the '${linksFromFile[0]}' link was failed`);
+    }
+
     for (let i = 1; i < randomLinks.length; i++) {
       await page.goto(randomLinks[i]);
       await bannerBtn.clickStartTradingBtnOnMainBanner();
       await expect(page.locator("#s_overlay > .form-container-white")).toBeVisible();
-      await expect(page.locator("#s_overlay").getByText("Sign up")).toHaveText(/Sign up/);
-      await expect(page.locator("#s_overlay").getByRole("link", { name: "Login" })).toBeVisible();
-      await expect(page.getByRole("textbox", { name: "Email address" })).toHaveAttribute("type", "email");
-      await expect(page.getByRole("textbox", { name: "Password" })).toHaveAttribute("type", "password");
-      await expect(page.getByRole("button", { name: "Continue" })).toHaveText(/Continue/);
-      await expect(page.locator("#s_overlay").getByRole("link", { name: "Privacy Policy" })).toBeVisible();
-      await page.getByRole("button", { name: "Cancel" }).click();
+      const elementText = await page.$eval('#s_overlay', element => element.innerText);
+      expect(elementText).toBeTruthy();
+      await expect(page.locator('[class="signup-form"] .h1')).toBeVisible();
+      await expect(page.locator('#s_overlay-email > .field__control')).toHaveAttribute("type", "email");
+      await expect(page.locator('#s_overlay-pass > .field__control')).toHaveAttribute("type", "password");
+      await expect(page.locator('.signup-form > .form-container-small-content > form > .btn')).toBeVisible();
+      await expect(page.locator('a[href="/terms-and-policies"]')).toBeVisible();
+      await page.locator('#s_overlay .form-container-white .button-cleared').click();
+
+      if (randomLinks.includes(randomLinks[i])) {
+        console.log(`Testing on the '${randomLinks[i]}' link was successfully completed `);
+      } else {
+        console.log(`Testing on the '${randomLinks[i]}' link was failed`);
+      }
+
     }
+
     // Функция для получения случайных элементов из массива
+
     function getRandomElements(array, count) {
       const randomized = array.slice();
       for (let i = randomized.length - 1; i > 0; i--) {
@@ -60,10 +87,10 @@ test.describe("US_11-02-02_Education > Menu item [Share trading] on UnReg Role",
       }
       return randomized.slice(0, count);
     }
+
   });
 
 })
- 
 
 
 
