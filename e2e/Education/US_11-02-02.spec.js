919d9547198c4acd7d19fd94f259ab78de3f58f2
Alexeeva Viktoria <tori-r@mail.ru>
2023-08-13 16:56:32 +0200

    us_11-02-02_(01-02-all) and refacoring us_11-03-04

--- e2e/Education/US_11-02-02.spec.js
+++ e2e/Education/US_11-02-02.spec.js
@@ -10,7 +10,7 @@ let bannerBtn;
 const language = "English";
 const country = "United Kingdom";
 
-test.describe("US_11-02-02_Education > Menu item [Share trading] on UnReg Role", () => {
+test.describe("US_11-02-02_Education > Menu item [Shares trading] on UnReg Role", () => {
 
   test.beforeAll(async ({ browser }) => {
     const context = await browser.newContext();
@@ -27,13 +27,18 @@ test.describe("US_11-02-02_Education > Menu item [Share trading] on UnReg Role",
     await header.hoverCountryAndLang()
     await page.getByRole("link", { name: language }).click();
     await header.getEducationMenu.hover();
-    await header.clickSharesTrading();
 
   });
 
   test(`TC_11.02.02_01_UnReg  > Test button [Start Trading] in Main banner on '${language}' language`, async () => {
     bannerBtn = new BannerBtn(page);
     const fs = require('fs');
+    if (await header.SharesTrading.isVisible()) {
+      await header.clickSharesTrading();
+    } else {
+      console.log(`For test on '${language}' language the page "Education->SharesTrading" doesn't exist on production`);
+      test.skip();
+    }
     await bannerBtn.clickStartTradingBtnOnMainBanner();
     await expect(page.locator("#s_overlay > .form-container-white")).toBeVisible();
     const elementText = await page.$eval('#s_overlay', element => element.innerText);
@@ -97,6 +102,12 @@ test.describe("US_11-02-02_Education > Menu item [Share trading] on UnReg Role",
   test(`TC_11.02.02_02_UnReg  > Test button [Try Demo] in Main banner on '${language}' language`, async () => {
     bannerBtn = new BannerBtn(page);
     const fs = require('fs');
+    if (await header.SharesTrading.isVisible()) {
+      await header.clickSharesTrading();
+    } else {
+      console.log(`For test on '${language}' language the page "Education->SharesTrading" doesn't exist on production`);
+      test.skip();
+    }
     await bannerBtn.clickTryDemoBtnOnMainBanner();
     await expect(page.locator("#s_overlay > .form-container-white")).toBeVisible();
     const elementText = await page.$eval('#s_overlay', element => element.innerText);
@@ -158,7 +169,7 @@ test.describe("US_11-02-02_Education > Menu item [Share trading] on UnReg Role",
 
 })
 
-test.describe("US_11-02-02_Education > Menu item [Share trading] on UnAuth Role", () => {
+test.describe("US_11-02-02_Education > Menu item [Shares trading] on UnAuth Role", () => {
   const testData = {
     email: "sadsass@gmail.com",
     password: "123Qwert!@dsdDs",
@@ -187,13 +198,18 @@ test.describe("US_11-02-02_Education > Menu item [Share trading] on UnAuth Role"
     await header.hoverCountryAndLang()
     await page.getByRole("link", { name: language }).click();
     await header.getEducationMenu.hover();
-    await header.clickSharesTrading();
 
   });
 
   test(`TC_11.02.02_01_UnAuth  > Test button [Start Trading] in Main banner on '${language}' language`, async () => {
     bannerBtn = new BannerBtn(page);
     const fs = require('fs');
+    if (await header.SharesTrading.isVisible()) {
+      await header.clickSharesTrading();
+    } else {
+      console.log(`For test on '${language}' language the page "Education->SharesTrading" doesn't exist on production`);
+      test.skip();
+    }
     await bannerBtn.clickStartTradingBtnOnMainBanner();
     try {
       await expect(page.locator("#l_overlay > .form-container-white")).toBeVisible();
@@ -273,6 +289,12 @@ test.describe("US_11-02-02_Education > Menu item [Share trading] on UnAuth Role"
   test(`TC_11.02.02_02_UnAuth  > Test button [Try Demo] in Main banner on '${language}' language`, async () => {
     bannerBtn = new BannerBtn(page);
     const fs = require('fs');
+    if (await header.SharesTrading.isVisible()) {
+      await header.clickSharesTrading();
+    } else {
+      console.log(`For test on '${language}' language the page "Education->SharesTrading" doesn't exist on production`);
+      test.skip();
+    }
     await bannerBtn.clickTryDemoBtnOnMainBanner();
     try {
       await expect(page.locator("#l_overlay > .form-container-white")).toBeVisible();
@@ -347,6 +369,167 @@ test.describe("US_11-02-02_Education > Menu item [Share trading] on UnAuth Role"
       return randomized.slice(0, count);
     }
 
+  });
+
+
+  test.describe("US_11-02-02_Education > Menu item [Shares trading] on Auth Role", () => {
+    const testData = {
+      email: "sadsass@gmail.com",
+      password: "123Qwert!@dsdDs",
+    }
+    test.beforeAll(async ({ browser }) => {
+      const context = await browser.newContext();
+      page = await context.newPage();
+      header = new Header(page);
+      login = new LoginPage(page);
+      // open capital.com
+      await page.goto("/");
+      // user unauthorization
+      await login.clickBtnLogIn();
+      await login.validLogin(testData.email, testData.password);
+      await login.continueButton.waitFor();
+      await login.continueButton.click();
+      await page.waitForLoadState('networkidle');
+      await page.goBack();
+      await page.waitForLoadState('networkidle');
+      // select country and language
+      await header.hoverCountryAndLang();
+      await page.getByRole("textbox").first().click();
+      await page.getByRole("link", { name: country }).click();
+      await header.hoverCountryAndLang()
+      await page.getByRole("link", { name: language }).click();
+      await header.getEducationMenu.hover();
+    });
+
+    test(`TC_11.02.02_01_Auth  > Test button [Start Trading] in Main banner on '${language}' language`, async () => {
+      bannerBtn = new BannerBtn(page);
+      const fs = require('fs');
+      if (await header.SharesTrading.isVisible()) {
+        await header.clickSharesTrading();
+      } else {
+        console.log(`For test on '${language}' language the page "Education->SharesTrading" doesn't exist on production`);
+        test.skip();
+      }
+      await bannerBtn.clickStartTradingBtnOnMainBanner();
+      await page.waitForTimeout(10000);
+      expect(await page).toHaveURL('https://capital.com/trading/platform/');
+      // expect(await page.locator('object.logo')).toBeVisible();
+      await page.waitForTimeout(10000);
+      await page.goBack();
+
+      console.log(`Testing the first level on the page 'https://capital.com/ar/trade-stocks ' completed successfully `)
+      await page.waitForTimeout(20000);
+      /* извлечение значения атрибута href (el.href) каждого элемента и добавление его в новый массив.Окончательный результат - массив links, 
+      содержащий все значения атрибута href выбранных элементов <a> */
+      const links = await page.$$eval('a[data-type="sidebar_deeplink"]', (elements) => elements.map((el) => el.href));
+      if (links.length === 0) {
+        console.log("There are no links on this page and testing of the second level is impossible");
+      } else {
+        console.log("links", links);
+      }
+      // запись элементов массива "links" в файл "links.txt" с использованием метода "writeFileSync" из модуля "fs"
+      fs.writeFileSync('links.txt', links.join('\n'));
+      // Содержимое файла "links.txt" считывается с использованием метода "readFileSync" из модуля "fs" и сохраняется в переменную "fileContent"
+      const fileContent = fs.readFileSync('links.txt', 'utf-8');
+      const linksFromFile = fileContent.split('\n').filter((link) => link !== '');
+      const randomLinks = getRandomElements(linksFromFile, 4);
+
+      for (let i = 1; i < randomLinks.length; i++) {
+        await page.goto(randomLinks[i]);
+        await bannerBtn.clickStartTradingBtnOnMainBanner();
+        await page.waitForTimeout(10000);
+        expect(await page).toHaveURL('https://capital.com/trading/platform/');
+        // expect(await page.locator('object.logo')).toBeVisible();
+        await page.waitForTimeout(10000);
+        await page.goBack();
+
+        if (randomLinks.includes(randomLinks[i])) {
+          console.log(`Testing on the '${randomLinks[i]}' link was successfully completed `);
+        } else {
+          console.log(`Testing on the '${randomLinks[i]}' link was failed`);
+        }
+
+      }
+
+      // Функция для получения случайных элементов из массива
+
+      function getRandomElements(array, count) {
+        const randomized = array.slice();
+        for (let i = randomized.length - 1; i > 1; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [randomized[i], randomized[j]] = [randomized[j], randomized[i]];
+        }
+        return randomized.slice(0, count);
+      }
+
+    });
+
+    test(`TC_11.02.02_02_Auth  > Test button [Try Demo] in Main banner on '${language}' language`, async () => {
+      bannerBtn = new BannerBtn(page);
+      const fs = require('fs');
+      if (await header.SharesTrading.isVisible()) {
+        await header.clickSharesTrading();
+      } else {
+        console.log(`For test on '${language}' language the page "Education->SharesTrading" doesn't exist on production`);
+        test.skip();
+      }
+      await bannerBtn.clickTryDemoBtnOnMainBanner();
+      await page.waitForLoadState('load');
+      await page.waitForLoadState('networkidle');
+      expect(await page).toHaveURL('https://capital.com/trading/platform/?mode=demo');
+      // expect(await page.locator('object.logo')).toBeVisible();
+      // expect(await page.locator('div.account__mode_demo')).toBeVisible();
+      await page.waitForTimeout(10000);
+      await page.goBack();
+
+      console.log(`Testing the first level on the page 'https://capital.com/ar/trade-stocks ' completed successfully `)
+      await page.waitForTimeout(20000);
+      /* извлечение значения атрибута href (el.href) каждого элемента и добавление его в новый массив.Окончательный результат - массив links, 
+      содержащий все значения атрибута href выбранных элементов <a> */
+      const links = await page.$$eval('a[data-type="sidebar_deeplink"]', (elements) => elements.map((el) => el.href));
+      if (links.length === 0) {
+        console.log("There are no links on this page and testing of the second level is impossible");
+      } else {
+        console.log("links", links);
+      }
+      // запись элементов массива "links" в файл "links.txt" с использованием метода "writeFileSync" из модуля "fs"
+      fs.writeFileSync('links.txt', links.join('\n'));
+      // Содержимое файла "links.txt" считывается с использованием метода "readFileSync" из модуля "fs" и сохраняется в переменную "fileContent"
+      const fileContent = fs.readFileSync('links.txt', 'utf-8');
+      const linksFromFile = fileContent.split('\n').filter((link) => link !== '');
+      const randomLinks = getRandomElements(linksFromFile, 4);
+
+      for (let i = 1; i < randomLinks.length; i++) {
+        await page.goto(randomLinks[i]);
+        await bannerBtn.clickTryDemoBtnOnMainBanner();
+        await page.waitForLoadState('load');
+        await page.waitForLoadState('networkidle');
+        expect(await page).toHaveURL('https://capital.com/trading/platform/?mode=demo');
+        // expect(await page.locator('object.logo')).toBeVisible();
+        // expect(await page.locator('div.account__mode_demo')).toBeVisible();
+        await page.waitForTimeout(10000);
+        await page.goBack();
+
+        if (randomLinks.includes(randomLinks[i])) {
+          console.log(`Testing on the '${randomLinks[i]}' link was successfully completed `);
+        } else {
+          console.log(`Testing on the '${randomLinks[i]}' link was failed`);
+        }
+
+      }
+
+      // Функция для получения случайных элементов из массива
+
+      function getRandomElements(array, count) {
+        const randomized = array.slice();
+        for (let i = randomized.length - 1; i > 1; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [randomized[i], randomized[j]] = [randomized[j], randomized[i]];
+        }
+        return randomized.slice(0, count);
+      }
+
+    });
   });
 
 })
