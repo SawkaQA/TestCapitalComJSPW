6410b0d2e13470ed482353d0b35887a545f6a75c
Alexeeva Viktoria <tori-r@mail.ru>
2023-08-15 21:23:51 +0200

    us_11-02-02_03

--- e2e/Education/US_11-03-04.spec.js
+++ e2e/Education/US_11-03-04.spec.js
@@ -7,8 +7,8 @@ const { BannerBtn } = require("../../pages/bannerButtons")
 let header;
 let page;
 let bannerBtn;
-const language = "English"
-const country = "United Kingdom";
+const language = "Español"
+const country = "Spain";
 
 test.describe("US_11-03-04_Education > Menu item [Position Trading]  on UnReg Role", () => {
 
